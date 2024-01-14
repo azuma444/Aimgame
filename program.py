ef1ffee9137174fcbb61fd91621150d215e01ae4
azuma <Metta483xtt4@gmail.com>
2024-01-14 20:10:33 +0900

    create 1 file and update 1 file

--- program.py
+++ program.py
@@ -1,2 +1,28 @@
 import pyxel
+import random
+
+class App:
+    def __init__(self):
+        pyxel.init(160, 120)
+        pyxel.load(images.pyxres)
+        self.x = random.randint(0,160)
+        self.y = random.randint(0,120)
+        self.exists = True
+        pyxel.mouse(True)
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        if pyxel.btnp(pyxel.MOUSE_LEFT_BUTTON):
+            self.exists = False
+
+        if self.exists == False:
+            return
+
+    def draw(self):
+        pyxel.cls(0)
+        if self.exists == False:
+            return
+        pyxel.blt(self.x, self.y, 0, 0, 0, 16, 16, 5)
+
+App()
 
