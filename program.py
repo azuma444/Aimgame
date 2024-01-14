c32bd450389fae8d1865aae31dd5fd2605905a20
azuma <Metta483xtt4@gmail.com>
2024-01-14 20:50:14 +0900

    delete 1 file and update 1 file

--- program.py
+++ program.py
@@ -4,10 +4,8 @@ import random
 class App:
     def __init__(self):
         pyxel.init(160, 120)
-        pyxel.load(images.pyxres)
         self.x = random.randint(0,160)
         self.y = random.randint(0,120)
-        self.exists = True
         pyxel.mouse(True)
         pyxel.run(self.update, self.draw)
 
@@ -15,14 +13,8 @@ class App:
         if pyxel.btnp(pyxel.MOUSE_LEFT_BUTTON):
             self.exists = False
 
-        if self.exists == False:
-            return
-
     def draw(self):
         pyxel.cls(0)
-        if self.exists == False:
-            return
         pyxel.blt(self.x, self.y, 0, 0, 0, 16, 16, 5)
 
 App()
-
