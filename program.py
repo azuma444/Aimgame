c6c589384916c04321dd739ff29b05ee7b97e6ad
azuma <Metta483xtt4@gmail.com>
2024-01-14 21:34:27 +0900

    update program.py

--- program.py
+++ program.py
@@ -4,21 +4,22 @@ import random
 class App:
     def __init__(self):
         pyxel.init(160, 120)
-        self.x = 0
-        self.y = 0
+        self.x = 12
+        self.y = 12
         pyxel.mouse(True)
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        self.x = random.randint(12,148)
-        self.y = random.randint(12,108)
-        pyxel.circ(random.randint(12,148), random.randint(12,108), 12, 8)
+        pass
 
     def draw(self):
         pyxel.cls(0)
+        pyxel.circ(self.x, self.y, 12, 8)
         if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
             pyxel.cls(0)
-            pyxel.circ(random.randint(12,148), random.randint(12,108), 12, 8)
+            self.x = random.randint(12,148)
+            self.y = random.randint(12,108)
+            pyxel.circ(self.x, self.y, 12, 8)
 
 
 App()
